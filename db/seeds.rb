3db36dfd50481e7ad012e0b46a08ade3c2fc29d5
RachelNolan <mail@rachelnolan.com>
2015-01-05 15:04:16 +0000

    Seeded database for resorts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+Resort.delete_all
+
+CSV.foreach("Resorts1.csv") do |col|
+  Resort.create(
+  	:resort => col[0],
+  	:country => col[1],
+  	:nearest_airport => col[2],
+  	:transfer_time => col[3],
+  	:size_of_ski_domain => col[4]
+  	)
+end
