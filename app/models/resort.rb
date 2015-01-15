7a740029b1a39c9a7fede51d60bf76ce124ecdb2
RachelNolan <mail@rachelnolan.com>
2015-01-15 17:20:29 +0000

    Refactored proportion_of(runs) method in Resort model.

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -6,16 +6,8 @@ class Resort < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :resort_name, use: :slugged
 
-  def proportion_easy_runs
-     self.easy_runs_km.to_f / self.size_of_ski_domain.to_f * 100
-  end
-
-  def proportion_medium_runs
-     self.medium_runs_km.to_f / self.size_of_ski_domain.to_f * 100
-  end
-
-  def proportion_hard_runs
-     self.hard_runs_km.to_f / self.size_of_ski_domain.to_f * 100
+  def proportion_of(runs)
+    runs.to_f / self.size_of_ski_domain.to_f * 100
   end
 
 end
